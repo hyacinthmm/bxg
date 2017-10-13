741a83dd3e267b0ea666b443dd76d264d26de890
lfa <lfa@itcast.cn>
2017-10-13 08:32:54 +0800

    vi

--- views/static/js/common.js
+++ views/static/js/common.js
@@ -7,7 +7,8 @@
 	// 	$(this).next().slideToggle();
 	// });
 
-
+define(["jquery","template","cookie"],function($,template){
+	
 	$(function(){
 		//如果不是在登录页面才需要从cookie中获取用户数据展示到页面
 		if(location.pathname != "/dashboard/login"){
@@ -18,5 +19,20 @@
 			var html=template("profile-tpl",userinfo);
 			$("#user-info").html(html);
 		}
+
+		//给退出登录按钮注册点击事件
+		$("#btn-logout").click=function(){
+			$.ajax({
+				url:"/api/logout",
+				type:"post",
+				success:function(data){
+					if(data.code==200){
+						location.href="/dashboard/login";
+					}
+				}
+			})
+			
+		}
 		
 	})
+})
